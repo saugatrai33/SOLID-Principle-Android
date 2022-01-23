06115f59aeb301ca132faa1b118cf7cf842eda14
Saugat Rai <saugatrai33@gmail.com>
2022-01-24 03:14:06 +0545

    open/close principle

--- app/src/main/java/com/example/solidprincipleandroid/FileLogger.kt
+++ app/src/main/java/com/example/solidprincipleandroid/FileLogger.kt
@@ -2,13 +2,29 @@ package com.example.solidprincipleandroid
 
 import java.io.File
 
-class FileLogger {
-
-    fun logError(err: Exception) {
+open class FileLogger {
+    open fun logError(err: Exception) {
         val file = File("errors.txt")
         file.appendText(
             text = err.message.toString()
         )
     }
+}
 
+class CustomErrorLogger : FileLogger() {
+    override fun logError(err: Exception) {
+        val file = File("custom_error_logger.txt")
+        file.appendText(
+            text = err.message.toString()
+        )
+    }
+}
+
+class GoodErrorLogger : FileLogger() {
+    override fun logError(err: Exception) {
+        val file = File("good_error_logger.txt")
+        file.appendText(
+            text = err.message.toString()
+        )
+    }
 }
