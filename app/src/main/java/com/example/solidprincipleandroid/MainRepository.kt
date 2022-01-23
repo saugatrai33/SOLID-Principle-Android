fde55d57ddf40ec66c73ae55b788ccb724657d76
Saugat Rai <saugatrai33@gmail.com>
2022-01-24 03:32:03 +0545

    updates

--- app/src/main/java/com/example/solidprincipleandroid/MainRepository.kt
+++ app/src/main/java/com/example/solidprincipleandroid/MainRepository.kt
@@ -6,21 +6,6 @@ class MainRepository(
     private val auth: FirebaseAuth,
     private val logger: FileLogger
 ) {
-    // two reasons to change this function
-    // sign in method & logging
-    // the correct version is given in next login method
-    suspend fun signIn(userName: String, password: String) {
-        try {
-            auth.signIn(userName, password)
-        } catch (e: Exception) {
-            val file = File("errors.txt")
-            file.appendText(
-                text = e.message.toString()
-            )
-        }
-    }
-
-    // only one reason to change i.e. login method only
     suspend fun logIn(userName: String, password: String) {
         try {
             auth.signIn(userName, password)
